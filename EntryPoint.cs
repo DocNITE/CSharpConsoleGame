de5bad2378aebef204fe091d9d6a66b15d53a188
DocNITE <docnite0530@gmail.com>
2023-05-25 22:53:22 +0300

    that prj

---
+++ EntryPoint.cs
@@ -0,0 +1,11 @@
+﻿using Engine;
+
+public class Game : Application
+{
+    public Game(Point ScreenSize, Point FontSize, string? Font = null) : base(ScreenSize, FontSize, Font) {
+    }
+
+    public override void Process() {
+        Screen.SetText(0,0,"lol");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,3 @@
+using Engine;
+
+var _game = new Game(new Point(70, 40), new Point(10, 10));
